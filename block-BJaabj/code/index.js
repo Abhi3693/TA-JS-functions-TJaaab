05f13ff62e3bd58f8d4b6f6ab8a6cbb1db80c297
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2021-09-09 15:19:06 +0530

    add assignment of writing function

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -2,6 +2,9 @@
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
 
+function sayhello () {
+ alert("hello world")
+}
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -11,6 +14,13 @@
   - Alert `fullName`
 */
 
+function fullName() {
+  let firstName = prompt("Enter your first name");
+  let lastName = prompt("Enter your last name");
+  let fullName = firstName + " " + lastName;
+  alert(fullName)
+}
+
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
 
@@ -19,6 +29,13 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
+
+function addTwoNumbers() {
+  let firstNum = +prompt("Enter the first name");
+  let secondNum = +prompt("Enter the second name");
+  let sum = firstNum + secondNum;
+  alert(sum)
+}
 
 /*
 4. Create a function named `getTable` with the following steps:
@@ -39,12 +56,33 @@
   9 * 10 = 90
 */
 
+function getTable() {
+  let num = +prompt("Enter a number");
+  let result = null;
+
+  for ( let i = 1; i <= 10; i++ ) {
+    let result = num * i;
+    console.log(result)
+  } 
+}
+
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+
+function isleapyear() {
+
+  let year = +prompt("Enter a year");
+  
+    if ( year % 4 == 0 ) {
+      alert("This is leap year");
+    } else {
+      alert("This is not leap year");
+    }
+  }
 
 /*
 6. Create a function named `getFactorial` with the following steps:
@@ -53,3 +91,12 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+
+function getFactorial() {
+let number = +prompt("Enter a number");
+let str = 1;
+  for ( let i = 1 ; i <= number; i++ ) {
+  str = str * i;
+  console.log(str)
+ }
+}
